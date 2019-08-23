373792662fd9dfae2004b60f967137a192f3e287
4903000 <4903000@chiru.no>
2019-08-23 17:39:50 +0300

    Fix database connection

--- console/db.php
+++ console/db.php
@@ -1,6 +1,6 @@
 <?php
 //variables
-$db_sock = "/home/kirisime/git/bantflags/guix-env/mysql/data/mysql.sock";
+$db_sock = "../guix-env/mysql/data/mysql.sock";
 $db_name = "flagsdb";
 $db_user = "flags";
 $db_pass = "default";
--- console/update_db.php
+++ console/update_db.php
@@ -6,7 +6,7 @@ include "db.php";
 $pdo_array=array("$old_name", "$new_name");
 try {
 	//pdo connection
-    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    $pdo = new PDO("mysql:unix_socket=$db_sock;dbname=$db_name", $db_user, $db_pass);
     //pdo query
     $sql = "UPDATE $db_table SET region = REPLACE (region, '$old_name', '$new_name') WHERE region  LIKE '%$old_name%'";
     $stm = $pdo->prepare($sql);
